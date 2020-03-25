dfe5b226da952c04c5dc1b7f44eba81b2a60fbf6
wazeer zulfikar <wazeer.zulfikar@gmail.com>
2020-03-25 00:10:24 -0400

    strictly INV interventions

--- count_inv.py
+++ count_inv.py
@@ -9,37 +9,44 @@ import os
 dataset_dir = 'ADReSS-IS2020-data/train/transcription/cc/'
 files = sorted(glob.glob(os.path.join(dataset_dir, '*.cha')))
 all_inv_counts_cc = []
-speaker_cc = []
 for filename in files:
     inv_count = 0
     with open(filename, 'r') as f:
         content = f.read().split('\n')
+        speaker_cc = []
 
         for c in content:
             if 'INV' in c:
-                speaker_cc.append(1)
-                inv_count+=1
+                speaker_cc.append('INV')
             if 'PAR' in c:
-                speaker_cc.append(0)
-                
+                speaker_cc.append('PAR')
+        
+        PAR_first_index = speaker_cc.index('PAR')
+        PAR_last_index = len(speaker_cc) - speaker_cc[::-1].index('PAR') - 1 
+        speaker_cc = speaker_cc[PAR_first_index:PAR_last_index]
+        inv_count = speaker_cc.count('INV')
     all_inv_counts_cc.append(inv_count)
     # print('{} has {} INVs'.format(filename.split('/')[-1], inv_count))
 
 dataset_dir = 'ADReSS-IS2020-data/train/transcription/cd/'
 files = sorted(glob.glob(os.path.join(dataset_dir, '*.cha')))
 all_inv_counts_cd = []
-speaker_cd = []
 for filename in files:
     inv_count = 0
     with open(filename, 'r') as f:
         content = f.read().split('\n')
+        speaker_cd = []
 
         for c in content:
             if 'INV' in c:
-                speaker_cd.append(1)
-                inv_count+=1
+                speaker_cd.append('INV')
             if 'PAR' in c:
-                speaker_cd.append(0)
+                speaker_cd.append('PAR')
+        
+        PAR_first_index = speaker_cd.index('PAR')
+        PAR_last_index = len(speaker_cd) - speaker_cd[::-1].index('PAR') - 1 
+        speaker_cd = speaker_cd[PAR_first_index:PAR_last_index]
+        inv_count = speaker_cd.count('INV')
     all_inv_counts_cd.append(inv_count)
     # print('{} has {} INVs'.format(filename.split('/')[-1], inv_count))
 
@@ -59,7 +66,7 @@ for threshold in range(10):
 
     precision = cd_pred / (cc_pred + cd_pred)
     recall = cd_pred / ((54-cd_pred) + cd_pred)
-    accuracy = (54-cc_pred+cd_pred)/108)
+    accuracy = (54-cc_pred+cd_pred)/108
     f1_score = (2*precision*recall)/(precision+recall)
     print('Accuracy {:.3f} '.format(accuracy))
     print('F1 score {:.3f}'.format(f1_score))
