34edc6756169db79eb5937df24c09b8841a2f223
wazeer zulfikar <wazeer.zulfikar@gmail.com>
2020-04-30 23:41:09 -0400

    other models

--- main.py
+++ main.py
@@ -4,8 +4,10 @@ import ensemble_infer
 def main():
 	dataset_dir = '../ADReSS-IS2020-data/train'
 	# model_dir = 'boosted_models_val_acc'
-	# model_dir = 'bagging_models_val_acc_1'
-	model_dir = 'boosted_models_val_acc_1'
+	model_dir = 'bagging_models_val_acc_1'
+	model_dir = 'bagging_models_val_acc_1'
+	# model_dir = 'boosted_models_val_acc_1'
+	model_dir = 'boosted_models_1'
 	n_splits = 5
 
 	# results = ensemble_trainer.bagging_ensemble_training(dataset_dir, model_dir, n_splits)
