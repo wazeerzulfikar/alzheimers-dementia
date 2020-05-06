57e3167fa79fa144b68c01ad6e405a16faa0884c
Utkarsh Sarawgi <usarawgi911@gmail.com>
2020-05-06 15:41:14 -0400

    latest

--- main_reg.py
+++ main_reg.py
@@ -7,7 +7,7 @@ def main():
 	dataset_dir = '../ADReSS-IS2020-data/train'
 
 	# model_dir = 'results/bagging_i_p_c'
-	model_dir = 'results/bagging_val_loss'
+	# model_dir = 'results/bagging_tuned_val_loss'
 	# model_dir = 'results/bagging_i_p_c_val_acc'
 	# model_dir = 'results/bagging_i_p_s_val_acc'
 
@@ -15,25 +15,32 @@ def main():
 	# model_dir = 'results/boosting_i_p_s_val_loss'
 	# model_dir = 'results/boosting_i_p_c_val_acc'
 	# model_dir = 'results/boosting_i_p_s_val_acc'
+	model_dir = 'results/bagging_val_loss'
+	model_dir = 'results/multi-head'
+	# model_dir = 'tuner/'
 
 	# model_dir = 'temp'
 
 	model_dir = Path(model_dir)
 	submodel_dirs = ['intervention', 'pause', 'spectogram', 'compare']
-	for m in submodel_dirs:
-		model_dir.joinpath(m+'/reg').mkdir(parents=True, exist_ok=True)
+	# for m in submodel_dirs:
+	# 	model_dir.joinpath(m).mkdir(parents=True, exist_ok=True)
 
 	model_dir = str(model_dir)
 
-	model_types = ['intervention', 'pause', 'compare']
+	model_types = ['pause']
+	model_types = ['pause-scratch']
+	# model_types = ['intervention', 'intervention-scratch']
+	# model_types = ['intervention-scratch']
+	model_types = ['intervention']
+	# model_types = ['compare']
 	# model_types = ['intervention', 'pause', 'spectogram']
 
 	n_splits = 5
 
-	results = ensemble_trainer_reg.bagging_ensemble_training(dataset_dir, model_dir, model_types, n_splits)
+	# results = ensemble_trainer_reg.bagging_ensemble_training(dataset_dir, model_dir, model_types, n_splits)
 
 	# Training order is same as model_types
-	# results = ensemble_trainer.boosted_ensemble_training(dataset_dir, model_dir, model_types, n_splits)
 
 	# dataset_split = 'full_dataset'
 	dataset_split = 'k_fold'
