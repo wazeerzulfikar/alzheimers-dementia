207690471678ad04ffa9560dfc4a34a059e24a11
rishabkhincha <khincharishab@gmail.com>
2020-05-21 12:42:25 +0530

    adding other datasets

--- regression_other/load_dataset.py
+++ regression_other/load_dataset.py
@@ -10,20 +10,38 @@ from sklearn.datasets import load_boston
 def load_dataset(config):
 
     if config.dataset=='boston':
-        print("Loading boston")
-        data_df = load_boston()
-        df = pd.DataFrame(data=data_df['data'], columns=data_df['feature_names'])
-        
-        X = df.values
-        y = data_df['target']
-        df['TARGET'] = data_df['target']
-        X1 = df[['ZN', 'INDUS', 'CHAS', 'NOX', 'RM', 'AGE', 'DIS', 'RAD', 'TAX']].values
-        X2 = df[['CRIM', 'PTRATIO', 'B', 'LSTAT']].values
-        y = df['TARGET']
-        
-        if config.mod_split=='none':
-            data = {'X':X, 'y':y}
-        elif config.mod_split=='human':
-            data = {'X1':X1, 'X2':X2, 'y':y}
+        data = _boston()        
 
+    elif config.dataset=='cement':
+        data = _cement()
+
+    elif config.dataset=='energy':
+        data = _cement()
+
+    elif config.dataset=='cement':
+        data = _cement()
+
+    return data
+
+def _boston(config):
+    print("Loading boston")
+    data_df = load_boston()
+    df = pd.DataFrame(data=data_df['data'], columns=data_df['feature_names'])
+    
+    X = df.values
+    y = data_df['target']
+    df['TARGET'] = data_df['target']
+    X1 = df[['ZN', 'INDUS', 'CHAS', 'NOX', 'RM', 'AGE', 'DIS', 'RAD', 'TAX']].values
+    X2 = df[['CRIM', 'PTRATIO', 'B', 'LSTAT']].values
+    y = df['TARGET']
+    
+    if config.mod_split=='none':
+        data = {'X':X, 'y':y}
+    elif config.mod_split=='human':
+        data = {'X1':X1, 'X2':X2, 'y':y}
+    return data
+
+def _cement(config):
+    print("Loading cement")
+    data=0
     return data
